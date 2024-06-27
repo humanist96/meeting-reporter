82653e3a96e0edf0286c2134063e5b35fe42374f
humanist96 <humanist96@gmail.com>
2024-06-27 16:25:09 +0900

    Update mm_st.py

--- mm_st.py
+++ mm_st.py
@@ -96,7 +96,7 @@ if 'api_key' not in st.session_state:
     st.session_state["newvalues"]=None
 
 # App title
-st.title("부동산 전망 쇼츠용 재작성 AI")
+st.title("부동산 게시글 교정 AI")
 
 with st.sidebar:
     st.markdown("""
