063da31b3bedab526f584e5a0476eb2082078980
humanist96 <humanist96@gmail.com>
2024-06-27 16:22:26 +0900

    Update mm_agent.py

--- mm_agent.py
+++ mm_agent.py
@@ -97,8 +97,7 @@ class CritiqueAgent:
         del short_article['source'] #to save tokens
         prompt = [{
             "role": "system",
-            "content": "You're a successful real estate investor and expert. Your sole purpose is to provide short feedback on a written "
-                       "article so the writer will know what to fix in korean\n "
+            "content": "You are a successful real estate investor and expert. Your sole purpose is to provide a rebuttal to an article written in opposition to your position and provide evidence. You respond in English so that we know what needs to be corrected.\n "
         }, {
             "role": "user",
             "content": f"Today's date is {datetime.now().strftime('%d/%m/%Y')}\n."
